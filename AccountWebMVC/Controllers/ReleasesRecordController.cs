c8632a1fe89c2492bf3ceb549343a64b71394746
Rodrigo <rodrigo.mori@imusica.com.br>
2020-11-22 23:08:29 -0300

    acerto

---
+++ AccountWebMVC/Controllers/ReleasesRecordController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountWebMVC.Controllers
+{
+    public class ReleasesRecordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ReleaseSearch()
+        {
+            return View();
+        }
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+
+    }
+}
