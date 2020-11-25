a7729dd8bb1be4465d0da1efb588414d548ed386
rodrigo <drigo_mori@hotmail.com>
2020-11-25 12:48:52 -0300

    acerto

--- AccountWebMVC/Controllers/ReleasesRecordController.cs
+++ AccountWebMVC/Controllers/ReleasesRecordController.cs
@@ -38,9 +38,21 @@ namespace AccountWebMVC.Controllers
             return View(result);
         }
 
-        public IActionResult GroupingSearch()
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
         {
-            return View();
+            if (!minDate.HasValue)
+            {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue)
+            {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+
+            var result = await _releaseRecordService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
 
     }
